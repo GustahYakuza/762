f592f33dd1b39c42f46b37326814820966a6f538
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 05:26:29 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,1 @@
-function login() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
 
-    // Simulação de lógica de login (substitua por uma autenticação real)
-    if (username === 'anna' && password === 'gatinha') {
-        // Salva o estado de autenticação
-        localStorage.setItem('isLoggedIn', 'true');
-
-        alert('Login bem-sucedido!');
-        
-        // Redireciona para a página inicial após o login
-        window.location.href = "index.html";
-    } else {
-        alert('Usuário ou senha incorretos. Tente novamente.');
-    }
-}
