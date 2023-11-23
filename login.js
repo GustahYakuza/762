faf5c16dad54ccc8d3d36fd44d77ab96015c058f
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 03:59:18 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Simulação de lógica de login (substitua por uma autenticação real)
+    if (username === 'seu_usuario' && password === 'sua_senha') {
+        alert('Login bem-sucedido!');
+        // Adicione redirecionamento ou lógica adicional após o login
+    } else {
+        alert('Usuário ou senha incorretos. Tente novamente.');
+    }
+}
