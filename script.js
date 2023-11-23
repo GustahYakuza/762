655e402be47a9c2c2e088784916293eac8b788a3
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 05:05:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ function showRandomCompliment() {
     const randomIndex = Math.floor(Math.random() * compliments.length);
     const compliment = compliments[randomIndex];
 
-    const complimentElement = document.getElementById('compliment');
-    complimentElement.innerHTML = `<span class="compliment-text">${compliment}</span><br><span class="emoji" id="heart"></span>`;
+    const complimentElement = document.querySelector('.compliment-text');
+    complimentElement.textContent = compliment;
 
     // Substitua o emoji pelo coração usando Twemoji
-    twemoji.parse(document.getElementById('heart'));
+    const heartEmoji = document.getElementById('heart');
+    heartEmoji.innerHTML = '';
+    twemoji.parse(heartEmoji, {
+        folder: 'svg',
+        ext: '.svg',
+    });
 }
 
 document.getElementById('changeCompliment').addEventListener('click', showRandomCompliment);
