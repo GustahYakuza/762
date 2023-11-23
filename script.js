d01d6fab8dcb121d47192ac087a5366b2dded653
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 05:22:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-// ... (seu código existente)
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-// Adicione essa função para carregar os elogios do arquivo JSON
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // Verifica as credenciais (usuário 'anna', senha 'gatinha' como exemplo)
+    if (username === 'anna' && password === 'gatinha') {
+        // Exibe o conteúdo principal após o login bem-sucedido
+        document.getElementById('loginForm').classList.add('hidden');
+        document.getElementById('mainContent').classList.remove('hidden');
+    } else {
+        alert('Credenciais inválidas. Tente novamente.');
+    }
+
+    // Limpa os campos de usuário e senha
+    usernameInput.value = '';
+    passwordInput.value = '';
+});
+
+let compliments = [];
+
+function showRandomCompliment() {
+    const randomIndex = Math.floor(Math.random() * compliments.length);
+    const compliment = compliments[randomIndex];
+
+    const complimentElement = document.querySelector('.compliment-text');
+    complimentElement.textContent = compliment;
+}
+
+// Adiciona essa função para carregar os elogios do arquivo JSON
 function loadCompliments() {
     fetch('compliments.json') // Verifique se o caminho para o arquivo está correto
         .then(response => response.json())
