fc25d4703a2c962e4d6c1d3b0a3436a87eda7c99
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 04:40:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // script.js
 
-// Array de elogios
 const compliments = [
     "Você é incrível!",
     "Seu sorriso ilumina meu dia!",
@@ -8,16 +7,14 @@ const compliments = [
     // Adicione mais elogios conforme desejado
 ];
 
-// Função para exibir um elogio aleatório
 function showRandomCompliment() {
     const randomIndex = Math.floor(Math.random() * compliments.length);
     const compliment = compliments[randomIndex];
 
-    document.getElementById('compliment').innerText = compliment;
+    const complimentElement = document.getElementById('compliment');
+    complimentElement.innerHTML = `<span class="compliment-text">${compliment}</span><br><i class="fas fa-heart"></i>`;
 }
 
-// Event listener para o botão de próximo elogio
 document.getElementById('changeCompliment').addEventListener('click', showRandomCompliment);
 
-// Chama a função para exibir um elogio inicial ao carregar a página
 showRandomCompliment();
